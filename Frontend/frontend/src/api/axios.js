6edd7a0940ccaafaa4760fc902a22d9ecc6b79a0
atreeverma <atreeverma17@gmail.com>
2026-04-10 20:34:40 +0530

    m

--- Frontend/frontend/src/api/axios.js
+++ Frontend/frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const api = axios.create({
-    baseURL: "http://localhost:8000/api/v1",
+    baseURL: "https://videostreamingplatform-7wc9.onrender.com/api/v1",
     withCredentials: true
 })
 export default api
